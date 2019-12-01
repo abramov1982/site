92d82114f16c964a7d4bacaf55512c24fe1041be
abramov1982 <49936583+abramov1982@users.noreply.github.com>
2019-12-01 16:40:04 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+
+$(".menu-item1").click(function(event) {
+		$(".all-about").hide('400', function() {});
+		$(".text-area1").show('400', function() {});
+	});
+
+$(".menu-item2").click(function(event) {
+		$(".all-about").hide('400', function() {});
+		$(".text-area2").show('400', function() {});
+	});
+
+$(".menu-item3").click(function(event) {
+		$(".all-about").hide('400', function() {});
+		$(".text-area3").show('400', function() {});
+	});
+
+});
